66f4cc095351c23da122662a0b97d8683dd55a28
Julian Rubisch <julian@julianrubisch.at>
2021-01-15 13:09:39 +0100

    Provide instance method

--- lib/cable_ready_callbacks.rb
+++ lib/cable_ready_callbacks.rb
@@ -1,7 +1,21 @@
 require "cable_ready_callbacks/version"
-require "cable_ready_callbacks/class_methods"
 require "cable_ready_callbacks/engine"
 
 module CableReadyCallbacks
-  # Your code goes here...
+  def cable_ready_morph
+    CableReadyCallbacks::StreamResourceJob.perform_later self
+  end
+
+  def self.included(mod)
+    class_eval do
+      channel = "#{mod.name.pluralize}Channel"
+      Object.const_set(channel, Class.new(ApplicationCable::Channel) {
+                                  def subscribed
+                                    resource_class = self.class.name.match(/\A(?<resource>\w*)Channel\Z/)[:resource].singularize.safe_constantize
+
+                                    stream_or_reject_for resource_class.find(params[:id])
+                                  end
+                                })
+    end
+  end
 end
--- lib/cable_ready_callbacks/engine.rb
+++ lib/cable_ready_callbacks/engine.rb
@@ -1,9 +1,9 @@
 module CableReadyCallbacks
   class Engine < ::Rails::Engine
-    initializer "connect active record to cable_ready_callbacks" do
-      ActiveSupport.on_load(:active_record) do
-        extend CableReadyCallbacks::ClassMethods
-      end
-    end
+    # initializer "connect active record to cable_ready_callbacks" do
+    #   ActiveSupport.on_load(:active_record) do
+    #     include CableReadyCallbacks::ClassMethods
+    #   end
+    # end
   end
 end
